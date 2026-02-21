584b20c32dbcd97e3928d7df13bfcc073db3d383
Illia-debug <dazle1994@gmail.com>
2026-02-21 17:27:38 +0300

    зробив правельний на мою думку перехід на юзер айді стор

---
+++ src/app/users/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+
+    return (
+        <div>
+            user content
+        </div>
+    );
+};
+
+export default Page;
--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,5 +1,6 @@
 import {IUser} from "@/model/IUser";
 import {FC} from "react";
+import Link from "next/link";
 
 type MyPropsType={
     user:IUser
@@ -9,6 +10,10 @@ export const User:FC<MyPropsType> = ({user}) => {
   return(
       <div>
           {user.name}
+          <div style={{
+              color:'red'
+          }}> <Link href={'/users/'+user.id}>user detail</Link> </div>
+
       </div>
   )
 }
