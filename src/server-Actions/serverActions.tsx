78af3053cda3f74532a31d6383d337e25b8d538e
Illia-debug <dazle1994@gmail.com>
2026-03-25 18:03:03 +0200

    пофіксив пост фетч-вивів в сервіс окремо

--- src/server-Actions/serverActions.tsx
+++ src/server-Actions/serverActions.tsx
@@ -1,21 +1,10 @@
 'use server'
-
 import {revalidatePath} from "next/cache";
-import {urls} from "@/constants/urls";
+import {postCarService} from "@/services/service.api";
 
 export const postCar=async (formData:FormData)=>{
-    //-------------------------------------------------
- await fetch(urls.cars.getAllCars, {
-    method: 'POST',
-    body: JSON.stringify({
-        brand: formData.get('brand'),
-        price: formData.get('price'),
-        year: formData.get('year')
-    }),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-    //________________________________________________
+
+    await postCarService(formData);
+
     revalidatePath('/')
 }
--- src/services/service.api.ts
+++ src/services/service.api.ts
@@ -6,3 +6,17 @@ export const getAllCar =async ():Promise<ICar[]> => {
         .then((response) => response.json())
 
 }
+export const postCarService =  async (data:FormData) => {
+     await  fetch(urls.cars.getAllCars, {
+        method: 'POST',
+        body: JSON.stringify({
+            brand: data.get('brand'),
+            price: data.get('price'),
+            year: data.get('year')
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    });
+}
